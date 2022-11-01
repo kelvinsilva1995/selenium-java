6e4626bdec021484b2f9d9e820d2805d670c8099
kelvin.silva <kelvin.silva@externo.cresol.coop.br>
2022-11-01 12:08:21 -0300

    ADD Hooks: BeforeClass e AfterClass

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -1,2 +1,33 @@
-package runner;public class RunCucumberTest {
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {},
+        features = "src/test/resources/features",
+        tags = "@desconto",
+        glue = {"steps"}
+)
+public class RunCucumberTest {
+    public static WebDriver driver ;
+
+    @BeforeClass
+    public static void stat(){
+        driver = new ChromeDriver();
+        System.out.println("Iniciou os Testes");
+    }
+
+    @AfterClass
+    public static void stop(){
+        System.out.println("Finalizou os Testes");
+        driver.quit();
+    }
 }
---
+++ src/test/resources/features/desconto.feature
@@ -0,0 +1,12 @@
+# language: pt
+  @desconto
+  Funcionalidade: Receber o cupom desconto da qazando
+    Eu como usuario da qazando
+    quero receber um cupom de desconto
+    para comprar um curso com valor reduzido
+
+  Cenario: Visualizar codigo de desconto
+    Dado que estou no site do qazando
+    Quando eu preencho meu e-mail
+    E clico em ganhar cupom
+    Então eu vejo o codigo de desconto
